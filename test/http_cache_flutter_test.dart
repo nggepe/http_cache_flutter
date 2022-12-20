7cd4b171d24473157d9b5fe23342a194c0c0424d
Gilang Pratama <gilang.pratama.banyuwangi@gmail.com>
2022-12-20 12:42:07 +0700

    code cov

--- test/http_cache_flutter_test.dart
+++ test/http_cache_flutter_test.dart
@@ -1,47 +1,44 @@
 import 'dart:convert';
-import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http_cache_flutter/http_cache_flutter.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:path/path.dart' as path;
 import 'package:http/http.dart' as http;
 
 import 'hc_request_test.mocks.dart';
+import 'http_cache_flutter_test.mocks.dart';
 
 const String url = "https://example.com";
 var grOutput = [
   {"nodeId": "1", "name": "hc", "fullName": "http cache"}
 ];
+final HttpCacheStorage storage = MockHttpCacheStorage();
+
 @GenerateNiceMocks([MockSpec<HttpCacheStorage>()])
 void main() {
   group("e2e test", () {
     TestWidgetsFlutterBinding.ensureInitialized();
-    setUp(() async {
-      HttpCache.storage = await HttpCache.init(
-          storageDirectory:
-              Directory(path.join(Directory.current.path, 'cache')));
+    setUpAll(() async {
+      HttpCache.storage = storage;
     });
 
-    tearDown(() async {
+    tearDownAll(() async {
       try {
-        // Directory(
-        //   path.join(Directory.current.path, 'cache'),
-        // ).deleteSync(recursive: true);
         await HttpCache.storage.clear();
-        // await HttpCacheStorage.hive.deleteFromDisk();
       } catch (_) {}
     });
 
-    testWidgets("render test", (tester) async {
+    testWidgets("render test from fetch", (tester) async {
       final client = MockClient();
       const String url = "https://example.com";
+
       when(
         client.get(Uri.parse(url)),
       ).thenAnswer((_) async {
         return http.Response(json.encode(grOutput), 200);
       });
+      when(storage.read(url)).thenAnswer((realInvocation) => null);
 
       await tester.pumpWidget(HttpCache<List<GithubRepository>>(
           clientSpy: client,
